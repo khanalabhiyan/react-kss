439fe4b15c30254cd4dd13f37ed7ae2dc6716621
abhiyan <abhiyankhanal@lftechnology.com>
2023-11-29 17:07:55 +0545

    with and without layouts

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -8,8 +8,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
 
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
@@ -43,7 +43,17 @@ function Layout() {
         </ul>
       </nav>
 
-      <hr />
+      <hr />   <p>
+        This example demonstrates some of the core features of React Router
+        including nested <code>&lt;Route&gt;</code>s,{" "}
+        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
+        "*" route (aka "splat route") to render a "not found" page when someone
+        visits an unrecognized URL.
+      </p>
+
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
 
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
@@ -87,3 +97,59 @@ function NoMatch() {
     </div>
   );
 }
+
+// import { Routes, Route, Link } from "react-router-dom";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Basic Example</h1>
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+        
+//         {/* Using path="*"" means "match anything", so this route
+//             acts like a catch-all for URLs that we don't have explicit
+//             routes for. */}
+//         <Route path="*" element={<NoMatch />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   );
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
+
+// function NoMatch() {
+//   return (
+//     <div>
+//       <h2>Nothing to see here!</h2>
+//       <p>
+//         <Link to="/">Go to the home page</Link>
+//       </p>
+//     </div>
+//   );
+// }
